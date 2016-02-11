047b5db47702f346c50a410f11bbbac5763beeec
Alessio Zappa <alessioedoardozappa@gmail.com>
2016-02-11 20:38:04 +0100

    other config options

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ const fs = require('fs');
 const jade = require('jade');
 const yaml = require('yamljs');
 const cons = require('better-console');
+const globule = require('globule');
+const yargs = require('yargs').argv;
 
 
+cons.info(`> Called script with these parameters: ${JSON.stringify(yargs)}`);
+
+
+const $t = function $t() { console.log(Array.apply(null, arguments)); return arguments[0]; };
+
+const t = function t(a, b) { console.log('a is: ', a, ' and b is: ', b); return a; };
+jade.filters.t = t;
+
+
+
+if (yargs.testing) {
+  const html = jade.renderFile(`./templates/index.jade`, {$t: $t});
+  console.log(html);
+  process.exit();
+}
+
+//  -- check if file or dir exists --
 const doesItExist = (s) => {
   try {
     return fs.realpathSync(s);
@@ -16,6 +35,7 @@ const doesItExist = (s) => {
 };
 
 
+//  -- config opts --
 if (!doesItExist(`./config.yaml`)) {
   cons.error(`> Error, you do not have a config file.`);
   process.exit();
@@ -31,6 +51,7 @@ if (!_config.langs || _config.langs.length < 1) {
 }
 
 
+//  -- locales --
 if (!doesItExist(`./locale`)) {
   cons.warn(`> Locale dir not found. Making dir now.`);
   fs.mkdirSync(`./locale`);
@@ -48,4 +69,16 @@ if (!_locales || _locales.length === 0) {
   });
 } else {
   cons.info('> You already got your files for translation.');
+  _config.langs.forEach(l => {
+    cons.info(`> Content for ${l} is `, yaml.load(`./locale/${l}.yaml`));
+  });
 }
+
+
+if (!doesItExist(_config.templateDir || `./templates`)) {
+  cons.error(`> Error, you don't set a template directory with jade files in your config.`);
+  process.exit();
+}
+
+const _templates = globule.find(`${(_config.templateDir || './templates')}/**/*.jade`);
+cons.info(_templates);
